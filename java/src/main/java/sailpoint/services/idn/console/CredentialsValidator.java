5e26fbd19ab803d5e19623c0efdee7a6394f54ee
adam.hampton <adam.hampton@sailpoint.com>
2018-04-26 15:58:44 -0500

    Interim commit while building out functionality.

---
+++ java/src/main/java/sailpoint/services/idn/console/CredentialsValidator.java
@@ -0,0 +1,17 @@
+package sailpoint.services.idn.console;
+
+public class CredentialsValidator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("");
+		System.out.println("IdentityNow Services SDK - Credentials Validation Utility.");
+		System.out.println("Copyright (C) 2018, SailPoint Technologies, Inc.");
+		System.out.println("All Rights Reserved.");
+		System.out.println("");
+		
+		// TODO: Connect up to the org and validate the credentials that are passed in.
+
+	}
+
+}

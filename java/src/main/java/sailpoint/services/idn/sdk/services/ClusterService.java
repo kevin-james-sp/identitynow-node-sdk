f94744164d5ed9533d9af9821890461b419dd3a7
Alexander.Strong <Alexander.Strong@sailpoint.com>
2018-08-19 21:02:19 -0500

    revert clusterList changes.

--- java/src/main/java/sailpoint/services/idn/sdk/services/ClusterService.java
+++ java/src/main/java/sailpoint/services/idn/sdk/services/ClusterService.java
@@ -4,13 +4,11 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
-import retrofit2.http.Headers;
 
 public interface ClusterService {
 
 
 	//Cannot be null, or the query will be dropped from the url. The ui sets the query to an empty string.
-	@Headers("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:61.0) Gecko/20100101 Firefox/61.0")
 	@GET( "/cc/api/cluster/list" )
 	Call<ResponseBody> clusterList (@Query("dc") long dc);
 }

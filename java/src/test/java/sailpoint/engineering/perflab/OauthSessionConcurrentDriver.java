6af16ae72d1b821915519126dca3654c3e195fc0
adam.hampton <adam.hampton@sailpoint.com>
2018-09-18 16:29:53 -0500

    Re-launch worker threads that have problems connecting or fail.

--- java/src/test/java/sailpoint/engineering/perflab/OauthSessionConcurrentDriver.java
+++ java/src/test/java/sailpoint/engineering/perflab/OauthSessionConcurrentDriver.java
@@ -44,108 +44,143 @@ public class OauthSessionConcurrentDriver {
 		AtomicInteger uiSessionCallCount = new AtomicInteger(0);
 		AtomicInteger failureCount = new AtomicInteger(0);
 		
+		AtomicInteger activeThreadCount = new AtomicInteger(0);
+		
 		ExecutorService es = Executors.newFixedThreadPool(numWorkerThreads);
 		
 		AtomicInteger threadStarupDelaySerial = new AtomicInteger(0);
 		
-		for (int i=0;i<=numWorkerThreads;i++) {
-			es.submit(() -> {
-				
-				int threadCount = 0;
-				
-				try {
-					Thread.sleep(25 * threadStarupDelaySerial.incrementAndGet());
-				} catch (Exception ex) {
-					log.error("Failure while waiting for thread start up delay.", ex);
-				}
-				
-				while (uiSessionCallCount.get() < numUiSessionCalls.get()) {
-						
-					long sessionStart = System.currentTimeMillis();
-					String ccSession = null;
+		// Note: see the while() conditions below.  We keep creating a new worker thread
+		// every 1/2 second while the worker thread activeThreadCount is less than the
+		// numWorkerThreads limit.  This way we re-spawn worker threads that have login
+		// failures the way that new users would try to 
+		do {
+			
+			// Spawn a new worker thread.
+			if (activeThreadCount.get() < numWorkerThreads) {
+				es.submit(() -> {
 					
-					int thisLoginCall= loginCallCount.incrementAndGet();
+					int activeId = activeThreadCount.incrementAndGet();
+					log.info("Launching new worker thread, active count:" + activeId);
 					
-					// Auto-close the uISession after every try() block.
-					try (UserInterfaceSession uiSession = (UserInterfaceSession) SessionFactory.createSession(SessionType.SESSION_TYPE_UI_USER_BASIC)) {
+					int threadCount = 0;
+					
+					/* Start-up delay handled by the do/while loop outside.
+					try {
+						Thread.sleep(25 * threadStarupDelaySerial.incrementAndGet());
+					} catch (Exception ex) {
+						log.error("Failure while waiting for thread start up delay.", ex);
+					}
+					*/
+					
+					while (uiSessionCallCount.get() < numUiSessionCalls.get()) {
+							
+						long sessionStart = System.currentTimeMillis();
+						String ccSession = null;
 						
-						UserInterfaceSession openOk = uiSession.open();
-						if (null == openOk) {
-							int failCount = failureCount.incrementAndGet();
-							log.error("Failure establising new CC session, failure ratio: " + failCount + "/" + loginCallCount.get());
-						} else {
-							ccSession = openOk.getUniqueId();
-						}
-						if (Boolean.parseBoolean(System.getProperty("skipUiSessionCall", "false"))) {
-							log.debug("Skipping stronglyAuthenticate() due to skipUiSessionCall setting.");
-						} else {
-							// uiSession.getNewSessionToken();
-							String newSession = uiSession.stronglyAuthenticate();
-							if (null == newSession) {
+						int thisLoginCall= loginCallCount.incrementAndGet();
+						
+						// Auto-close the uISession after every try() block.
+						try (UserInterfaceSession uiSession = (UserInterfaceSession) SessionFactory.createSession(SessionType.SESSION_TYPE_UI_USER_BASIC)) {
+							
+							UserInterfaceSession openOk = uiSession.open();
+							if (null == openOk) {
 								int failCount = failureCount.incrementAndGet();
-								log.error("Failure establising strongly authenticated CC session, failure ratio: " + failCount + "/" + loginCallCount.get());
+								log.error("Failure establising new CC session, failure ratio: " + failCount + "/" + loginCallCount.get());
 							} else {
-								ccSession = uiSession.getUniqueId();
+								ccSession = openOk.getUniqueId();
+							}
+							if (Boolean.parseBoolean(System.getProperty("skipUiSessionCall", "false"))) {
+								log.debug("Skipping stronglyAuthenticate() due to skipUiSessionCall setting.");
+							} else {
+								// uiSession.getNewSessionToken();
+								String newSession = uiSession.stronglyAuthenticate();
+								if (null == newSession) {
+									int failCount = failureCount.incrementAndGet();
+									log.error("Failure establising strongly authenticated CC session, failure ratio: " + failCount + "/" + loginCallCount.get());
+								} else {
+									ccSession = uiSession.getUniqueId();
+								}
+								
+								LongSummaryStatistics localStats = new LongSummaryStatistics();
+								
+								for (int uiSessionCalls=0; uiSessionCalls<numUiSessionCalls.get(); uiSessionCalls++) {
+									
+									long startTime = System.currentTimeMillis();
+									String multiCallStr = uiSession.getNewSessionToken();
+									long duration = System.currentTimeMillis() - startTime;
+									
+									// Handle a non-200 response here.
+									if (null == multiCallStr) {
+										log.error("Got a non-200 response, shutting down this thread.");
+										break; // out of for() loop.
+									}
+								
+									localStats.accept(duration);
+									
+									// Warn if the duration takes a long time, give it 1/2 a second.
+									if (duration > 5000) {
+										log.warn("Long call to /ui/session - duration: " + duration + " msecs for session:" + multiCallStr);
+									}
+									
+									int thisGlobalCall = uiSessionCallCount.incrementAndGet();
+									
+									if ((uiSessionCalls % 5) == 0) {
+										String appList = uiSession.doApiGet("/cc/api/app/list");
+										log.debug("cc/api/app/list:" + appList);
+									}
+									
+									if ((uiSessionCalls % 3) == 0) {
+										String appList = uiSession.doApiGet("/cc/api/user/status");
+										log.debug("cc/api/user/status:" + appList);
+									}
+									
+									if ((uiSessionCalls % 50) == 0) {
+										String statsStr = String.format("%d/%d/%f", localStats.getMin(), localStats.getMax(), localStats.getAverage());
+										log.info("Completed local " + uiSessionCalls + ", global:" + thisGlobalCall + " stats m/x/a:" + statsStr);
+										localStats = new LongSummaryStatistics();
+									}
+									
+								}
+								
 							}
 							
-							LongSummaryStatistics localStats = new LongSummaryStatistics();
-							
-							for (int uiSessionCalls=0; uiSessionCalls<numUiSessionCalls.get(); uiSessionCalls++) {
-								
-								long startTime = System.currentTimeMillis();
-								String multiCallStr = uiSession.getNewSessionToken();
-								long duration = System.currentTimeMillis() - startTime;
-								
-								// Handle a non-200 response here.
-								if (null == multiCallStr) {
-									log.error("Got a non-200 response, shutting down this thread.");
-									break; // out of for() loop.
-								}
-							
-								localStats.accept(duration);
-								
-								// Warn if the duration takes a long time, give it 1/2 a second.
-								if (duration > 5000) {
-									log.warn("Long call to /ui/session - duration: " + duration + " msecs for session:" + multiCallStr);
-								}
-								
-								int thisGlobalCall = uiSessionCallCount.incrementAndGet();
-								
-								if ((uiSessionCalls % 3) == 0) {
-									String appList = uiSession.doApiGet("/cc/api/app/list");
-									log.debug("cc/api/app/list:" + appList);
-								}
-								
-								if ((uiSessionCalls % 50) == 0) {
-									String statsStr = String.format("%d/%d/%f", localStats.getMin(), localStats.getMax(), localStats.getAverage());
-									log.info("Completed local " + uiSessionCalls + ", global:" + thisGlobalCall + " stats m/x/a:" + statsStr);
-									localStats = new LongSummaryStatistics();
-								}
-								
-							}
-							
+						} catch (IOException e) {
+							int failCount = failureCount.incrementAndGet();
+							log.error("Failure establising new CC session, failure ratio: " + failCount + "/" + loginCallCount.get(), e);
+							activeThreadCount.decrementAndGet();
+							return;
 						}
 						
-					} catch (IOException e) {
-						int failCount = failureCount.incrementAndGet();
-						log.error("Failure establising new CC session, failure ratio: " + failCount + "/" + loginCallCount.get(), e);
-						return;
+						if (null != ccSession) {
+							long sessionSetup = System.currentTimeMillis() - sessionStart;
+							log.info("Call " + thisLoginCall + " successfully authenticated to CC session in " + sessionSetup + " msecs, CCSESSIONID:" + ccSession);
+						}
+						
+						threadCount++;
+					
 					}
 					
-					if (null != ccSession) {
-						long sessionSetup = System.currentTimeMillis() - sessionStart;
-						log.info("Call " + thisLoginCall + " successfully authenticated to CC session in " + sessionSetup + " msecs, CCSESSIONID:" + ccSession);
+					log.info("Thread exited @ theadCount:" + threadCount + "  callCount:" + loginCallCount.get() + " failureCount:" + failureCount.get());
+					if (uiSessionCallCount.get() >= numUiSessionCalls.get()) {
+						es.shutdown();
 					}
-					
-					threadCount++;
-				
-				}
-				
-				log.info("Thread exited @ theadCount:" + threadCount + "  callCount:" + loginCallCount.get() + " failureCount:" + failureCount.get());
-				es.shutdown();
-			});
+					activeThreadCount.decrementAndGet();
+				});
+
+			}
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		
-		}
+		} while (
+				(activeThreadCount.get() < numWorkerThreads) && 
+				(uiSessionCallCount.get() < numUiSessionCalls.get())
+		);
 		
 		try {
 			es.awaitTermination(1, TimeUnit.DAYS);

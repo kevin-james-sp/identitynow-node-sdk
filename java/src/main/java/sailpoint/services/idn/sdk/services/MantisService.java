fa5ee75d0a0a5082959a3fb3698a6e628a4cc4da
Alexander.Strong <Alexander.Strong@sailpoint.com>
2018-09-27 21:07:43 -0500

    updated paths

--- java/src/main/java/sailpoint/services/idn/sdk/services/MantisService.java
+++ java/src/main/java/sailpoint/services/idn/sdk/services/MantisService.java
@@ -2,9 +2,9 @@ package sailpoint.services.idn.sdk.services;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
@@ -15,40 +15,47 @@ import java.util.Map;
 public interface MantisService {
 
 	//no mantis calls will work, fml
-	@FormUrlEncoded
-	@POST("/debug/v2/mantis/post?path=sources%2Fsources%2FaddAttributeToGroupSchema")
-	Call<ResponseBody> addAttributeToGroupSchema(@Field ("externalId") String externalId,
-	                                             @Field("schemaAttributeRequest") String schemaAttributeRequest);
-
-	@POST("/debug/v2/mantis/post?path=sources%2Fsources%2FdeleteAttributesFromGroupSchema")
-	Call<ResponseBody> deleteAttributesFromGroupSchema(@Field("externalId") String externalId,
-	                                                   @Field("schemaAttributeRequest") Map schemaAttributeRequest);
-
-	@POST("/debug/v2/mantis/post?path=sources%2Fsources%2FdeleteGroupSchema")
-	Call<ResponseBody> deleteGroupSchema(@Field("externalId") String externalId);
-
-	@GET("/debug/v2/mantis/post?path=sources%2Fsources%2F{externalId}%2FbuildMapRule")
-	Call<ResponseBody> getBuildMapRule(@Path("externalId") String externalId);
-
-	@GET("/debug/v2/mantis/post?path=sources%2Fsources%2F{externalId}%2FJDBCProvisionRule")
-	Call<ResponseBody> getJDBCProvisionRule(@Path("externalId") String externalId);
 
 	@Headers("Content-Type:application/json")
-	@POST("/debug/v2/mantis/post?path=sources%2Fsources%2F{externalId}%2FbuildMapRule")
-	Call<ResponseBody> postBuildMapRule(@Path("externalId") String externalId,
-	                                    @Field("sourceCode") String sourceCode);
+	@POST("/mantis/sources/schema/{sourceId}/addAttributeToGroupSchema")
+	Call<ResponseBody> addAttributeToGroupSchema(@Path ("sourceId") String sourceId,
+	                                             @Body Map schemaAttributeRequest);
 
-	@POST("/debug/v2/mantis/post?path=sources%2Fsources%2F{externalId}%2FJDBCProvisionRule")
-	Call<ResponseBody> postJDBCProvisionRule(@Path("externalId") String externalId,
-	                                         @Field("sourceCode") String sourceCode);
+	@POST("/mantis/sources/sources/{sourceId}")
+	Call<ResponseBody> updateApplication(@Path ("sourceId") String sourceId,
+	                                     @Body Map attributes);
 
-	@DELETE("/debug/v2/mantis/post?path=sources%2Fsources%2F{externalId}%2FbuildMapRule")
-	Call<ResponseBody> deleteBuildMapRule(@Path("externalId") String externalId);
+	@POST("/mantis/sources/schema/{sourceId}/deleteAttributesFromGroupSchema")
+	Call<ResponseBody> deleteAttributesFromGroupSchema(@Path("sourceId") String sourceId,
+	                                                   @Field("schemaAttributeRequest") Map schemaAttributeRequest);
 
-	@DELETE("/debug/v2/mantis/post?path=sources%2Fsources%2F{externalId}%2FJDBCProvisionRule")
-	Call<ResponseBody> deleteJDBCProvisionRule(@Path("externalId") String externalId);
+	@POST("/mantis/sources/schema/{sourceId}/deleteGroupSchema")
+	Call<ResponseBody> deleteGroupSchema(@Path("sourceId") String sourceId);
 
-	@POST("/debug/v2/mantis/post?path=provisioning%2FprovisioningPolicies%2F{externalId}%2FdiscoverCreatePolicy")
-	Call<ResponseBody> discoverCreatePolicy(@Path("externalId") String externalId);
+	@GET("/mantis/sources/sources/{sourceId}/buildMapRule")
+	Call<ResponseBody> getBuildMapRule(@Path("sourceId") String sourceId);
+
+	@GET("/mantis/sources/sources/{sourceId}/JDBCProvisionRule")
+	Call<ResponseBody> getJDBCProvisionRule(@Path("sourceId") String sourceId);
+
+	@Headers("Content-Type:application/json")
+	@POST("/mantis/sources/sources/{sourceId}/buildMapRule")
+	Call<ResponseBody> postBuildMapRule(@Path("sourceId") String sourceId,
+	                                    @Body String sourceCode);
+
+	@Headers("Content-Type:application/json")
+	@POST("/mantis/sources/sources/{sourceId}/JDBCProvisionRule")
+	Call<ResponseBody> postJDBCProvisionRule(@Path("sourceId") String sourceId,
+	                                         @Body String sourceCode);
+
+	@DELETE("/mantis/sources/sources/{sourceId}/buildMapRule")
+	Call<ResponseBody> deleteBuildMapRule(@Path("sourceId") String sourceId);
+
+	@DELETE("/mantis/sources/sources/{sourceId}/JDBCProvisionRule")
+	Call<ResponseBody> deleteJDBCProvisionRule(@Path("sourceId") String sourceId);
+
+	@Headers("Content-Type:application/json")
+	@POST("/mantis/provisioning/provisioningPolicies/{sourceId}/discoverCreatePolicy")
+	Call<ResponseBody> discoverCreatePolicy(@Path("sourceId") String sourceId);
 
 }

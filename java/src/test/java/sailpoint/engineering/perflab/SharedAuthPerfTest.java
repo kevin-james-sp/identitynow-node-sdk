b795866bb648315b78f6ee47ade177b623d8e062
Alexander.Strong <Alexander.Strong@sailpoint.com>
2018-11-30 16:14:52 -0600

    Added FF's, and more stubs in the test

--- java/src/test/java/sailpoint/engineering/perflab/SharedAuthPerfTest.java
+++ java/src/test/java/sailpoint/engineering/perflab/SharedAuthPerfTest.java
@@ -1,4 +1,14 @@
 package sailpoint.engineering.perflab;
 
+import org.junit.Test;
+import sailpoint.services.idn.sdk.services.FeatureFlagService;
+
 public class SharedAuthPerfTest {
+
+	private FeatureFlagService _ffService;
+
+	@Test
+	public void compareAuthServices(){
+
+	}
 }

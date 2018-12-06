9b822dbae7507c82e3d70c52f9288fa6f1faffc9
Alexander.Strong <Alexander.Strong@sailpoint.com>
2018-12-06 15:35:05 -0600

    Thread stubs

---
+++ java/src/test/java/sailpoint/concurrent/threads/TwoMfaCodeThread.java
@@ -0,0 +1,9 @@
+package sailpoint.concurrent.threads;
+
+public class TwoMfaCodeThread implements Runnable {
+
+	@Override
+	public void run() {
+
+	}
+}
---
+++ java/src/test/java/sailpoint/concurrent/threads/TwoMfaKbaThread.java
@@ -0,0 +1,9 @@
+package sailpoint.concurrent.threads;
+
+public class TwoMfaKbaThread implements Runnable {
+
+	@Override
+	public void run() {
+
+	}
+}

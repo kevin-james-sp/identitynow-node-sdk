ab5f3cc81a8d1710955a8e7fedab80a963442279
Alexander.Strong <Alexander.Strong@sailpoint.com>
2019-03-20 13:28:20 -0500

    Added client/secret

---
+++ java/src/test/java/sailpoint/engineering/perflab/IDARecommenderDriver.java
@@ -0,0 +1,8 @@
+package sailpoint.engineering.perflab;
+
+public class IDARecommenderDriver {
+
+	String ClientId = "35872355-6096-4b21-b0b2-7d9b814ff790";
+	String secret = "c62fb62724bc46a080d313edc7ef5131c3476513cbb417e01d18806eeca0e30e";
+
+}

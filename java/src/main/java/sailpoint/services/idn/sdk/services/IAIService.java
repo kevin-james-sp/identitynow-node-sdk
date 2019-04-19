7c5a4e46aa6849aa7b69a81b19ceee28f1967b56
adam.hampton <adam.hampton@sailpoint.com>
2019-04-19 14:52:27 -0500

    Changing the service response type to get JSon parsing correctly.

--- java/src/main/java/sailpoint/services/idn/sdk/services/IAIService.java
+++ java/src/main/java/sailpoint/services/idn/sdk/services/IAIService.java
@@ -6,12 +6,12 @@ import retrofit2.http.Header;
 import retrofit2.http.POST;
 import sailpoint.services.idn.sdk.object.IAI.Oauth.AccessToken;
 import sailpoint.services.idn.sdk.object.IAI.recommender.RecommenderFields;
-import sailpoint.services.idn.sdk.object.IAI.recommender.ResponseElement;
+import sailpoint.services.idn.sdk.object.IAI.recommender.Responses;
 
 public interface IAIService {
 
 	@POST( "beta/recommendations/request" )
-	Call<ResponseElement> recommendationRequest (@Header ("Authorization") String authorization,
+	Call<Responses> recommendationRequest (@Header ("Authorization") String authorization,
 	                                             @Header("Content-Type") String contentType,
 	                                             @Body RecommenderFields recommenderFields);
 

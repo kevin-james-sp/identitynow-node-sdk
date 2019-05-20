092a3444e7bb9d13a50226f4b45ef3ddc09275e4
Fangming Ning <fangming.ning@sailpoint.com>
2019-05-20 14:57:00 -0500

    Changing code in main for the retrofit version of the search

--- java/src/main/java/sailpoint/services/idn/sdk/scaffolding/SearchScaffolding.java
+++ java/src/main/java/sailpoint/services/idn/sdk/scaffolding/SearchScaffolding.java
@@ -6,10 +6,11 @@ import java.util.List;
 import org.apache.logging.log4j.Level;
 
 import sailpoint.services.idn.console.Log4jUtils;
+import sailpoint.services.idn.sdk.EnvironmentCredentialer;
+import sailpoint.services.idn.sdk.IdentityNowService;
 import sailpoint.services.idn.sdk.object.Identity;
-import sailpoint.services.idn.sdk.services.SearchIdentityService;
-import sailpoint.services.idn.session.SessionBase;
-import sailpoint.services.idn.session.SessionFactory;
+import sailpoint.services.idn.sdk.services.SearchService;
+import sailpoint.services.idn.session.SessionType;
 
 /**
  * Scaffolding infrastructure to support accessing the Search service.
@@ -19,29 +20,19 @@ import sailpoint.services.idn.session.SessionFactory;
 public class SearchScaffolding {
 
 	public static void main(String[] args) {
-		
+
 		Log4jUtils.boostrapLog4j(Level.DEBUG);
-		
-		SessionBase session = SessionFactory.createApiSession();
-		try {
-			session.open();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		SearchIdentityService srchIdSvc = new SearchIdentityService(session);
-		
-		List<Identity> idList = srchIdSvc.search("id=99999");
-		
-		try {
-			session.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+        try {
+            IdentityNowService ids = new IdentityNowService(EnvironmentCredentialer.getEnvironmentCredentials());
+            ids.createSession(SessionType.SESSION_TYPE_UI_USER_BASIC, true);
+            SearchService searchService = ids.getSearchService();
+
+            List<Identity> idList = searchService.searchIdentities(50, 0, "id=99999").execute().body();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	
 	}
-	
-	
-
 }

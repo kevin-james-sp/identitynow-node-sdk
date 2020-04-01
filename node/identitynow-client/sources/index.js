34ff5afa036adeda0ad9831b89701e77c41f56fd
Kev James <kevin.james@sailpoint.com>
2020-04-01 23:48:02 +0100

    Sources: list now uses paging, 'get' implemented

--- node/identitynow-client/sources/index.js
+++ node/identitynow-client/sources/index.js
@@ -4,31 +4,57 @@ function Sources( client ) {
 
     this.client=client;
 
+    this.getPage=function(off, lst) {
+        
+        let offset=0;
+        if (off!=null) {
+            offset=off;
+        }
+
+        let list=[];
+        if (lst!=null) {
+            list=lst;
+        }
+
+        let limit=10;
+    
+        let url=this.client.apiUrl+'/beta/sources?limit='+limit+'&offset='+offset+'&count=true';
+    
+        let that=this;
+
+        return this.client.get(url)
+            .then( function (resp) {
+            count=resp.headers['x-total-count'];
+            resp.data.forEach( function( itm ) {
+                list.push(itm);
+            } );
+            offset+=resp.data.length;
+            if (list.length<count) {
+                return that.getPage(offset, list);
+            }
+            return Promise.resolve(list);
+        });
+    
+    
+    }
+
 }
+
 
 Sources.prototype.list = function list () {
 
-    let limit=100;
-    let offset=0;
-
-    var url=this.client.apiUrl+'/beta/sources?limit='+limit+'&offset='+offset+'&count=true';
-
-    return this.client.get(url)
-    .then( function (resp) {
-        return Promise.resolve(resp.data);
-    }).catch( function (err) {
-        console.log(err);
-        return Promise.resolve(err);
-    });
+    return this.getPage();
 
 }
 
-Sources.prototype.get = async function get ( id ) {
+Sources.prototype.get = function get ( id ) {
     
-    var token=await this.client.token();
-    return {
-        "a": "thing"
-    }
+    let url=this.client.apiUrl+'/beta/sources/'+id;
+    
+    return this.client.get(url)
+        .then( function (resp) {
+            return Promise.resolve(resp.data);
+        } );
 }
 
 module.exports = Sources;
